c500c77c28158aae3c412c4b38186ddbc07a0132
Brad Newman <newmanbradm@gmail.com>
2018-03-06 00:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if(string === string.toUpperCase()) return "I can't hear you!" ;
-  else if(string === string.toLowerCase()) return ""
+  else if(string === string.toLowerCase()) return "I can't hear you!"
 }
